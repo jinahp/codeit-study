595556409ea03ce0a4ff9ac4f776826ba1fef497
Jinah <monsatfri@gmail.com>
2024-01-02 23:05:41 +0900

    입력 폼 다루기 - 04. 하나의 state로 폼 구현하기

--- codeit-react-moviepedia/src/components/App.js
+++ codeit-react-moviepedia/src/components/App.js
@@ -5,14 +5,31 @@
  * useEffect 함수에 실행할 콜백 함수와 빈 배열을 넘겨주면 리액트는 콜백 함수를 맨 처음 렌더링 할 때만 실행한다.
  * 따라서, 무한 루프에 빠지지 않는다.
  *
+ * useEffect 내의 콜백 함수는 리액트가 비동기로 실행하는 함수이다.
+ * 배열은 dependency list라고 부르며, 이 배열에 있는 값이 바뀔 때마다 콜백 함수를 실행한다.
+ * 배열에 아무 값도 넣지 않으면 리액트는 콜백 함수를 맨 처음 렌더링 할 때만 실행한다.
+ *
+ * useEffect 함수는 맨 처음 렌더링이 끝나면 콜백 함수를 실행하고,
+ * 그 다음부터는 dependency list를 비교하여 기억한 값이 다른 경우에만 콜백을 실행한다.
+ *
+ * 비동기로 state를 변경할 때는 잘못된 시점의 값을 사용하는 문제가 있다.
+ * 그럴 땐, setter 함수의 값이 아니라 콜백을 전달해서 해결할 수 있다.
+ * setter함수((이전 state값) => [...변경할 state값, ...reviews]);
+ *
  */
-
 import { useEffect, useState } from "react";
 import ReviewList from "./ReviewList";
+import ReviewForm from "./ReviewForm";
 import { getReviews } from "../api";
+
+const LIMIT = 6;
 
 function App() {
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
   const [items, setItems] = useState([]);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -25,14 +42,36 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoad = async () => {
-    const { reviews } = await getReviews();
-    setItems(reviews);
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      setLoadingError(null);
+      setIsLoading(true);
+      result = await getReviews(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+
+    const { paging, reviews } = result;
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems((prevItems) => [...prevItems, ...reviews]);
+    }
+    setOffset(options.offset + options.limit);
+    setHasNext(paging.hasNext);
+  };
+
+  const handleLoadMore = async () => {
+    await handleLoad({ order, offset, limit: LIMIT });
   };
 
   useEffect(() => {
-    handleLoad();
-  }, []);
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order]);
 
   return (
     <div>
@@ -40,7 +79,14 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
       </div>
+      <ReviewForm />
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </div>
   );
 }
---
+++ codeit-react-moviepedia/src/components/ReviewForm.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * [정리]
+ * onChange 이벤트는 사용자 입력이 끝났을 때 발생한다.
+ * 리액트에서 onChange는 순수 자바스크립트에서 사용하는 onChange와는 다르다.
+ * 리액트에서 onChange는 input의 value가 변경될 때마다 발생한다.
+ * input의 value가 변경될 때마다 그 값을 state에 반영한다.
+ *
+ * onSubmit 이벤트는 form에서 submit 버튼을 눌렀을 때 발생한다.
+ * 기본 동작 방해하기 - e.preventDefault();
+ *
+ * 입력 폼에서 여러 개로 관리하던 state를 하나의 state로 관리하는 방법은 input 태그의 name 속성을 활용하는 것이다.
+ *
+ *
+ */
+
+import { useState } from "react";
+import "./ReviewForm.css";
+
+function ReviewForm() {
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target; // e.target에서 name과 value를 가져온다.
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value, // name의 값으로 프로퍼티명 지정하고 value를 할당한다.
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
+  return (
+    <form className="ReviewForm" onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input
+        type="number"
+        name="rating"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea name="content" value={values.content} onChange={handleChange} />
+      <button type="submit">확인</button>
+    </form>
+  );
+}
+
+export default ReviewForm;
